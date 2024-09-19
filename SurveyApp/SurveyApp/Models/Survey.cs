09123f11ec4f91b9a211db2cea0fcaa599997ba5
Emre Taştekin <112701080+emretastekin@users.noreply.github.com>
2024-09-19 22:26:27 +0300

    Survey--System
    Developed with C#

---
+++ SurveyApp/SurveyApp/Models/Survey.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SurveyApp.Models
+{
+    public class Survey
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = "";
+
+        [Required]    
+        public string Surname { get; set; } = "";
+
+        [EmailAddress]
+        public string Email { get; set; } = "";
+
+        [Phone]
+        [StringLength(15)]
+        public string PhoneNumber { get; set; } = "";
+
+        [StringLength(200)]
+        public string? Answer1 { get; set; }
+
+        public string? ImagePath { get; set; }
+
+        public int? Rating { get; set; }
+
+        public bool? YesNoAnswer { get; set; }
+    
+    }
+}
